4a7229e11f09be218b70205977ec4f437e3a0f14
University <inonuni@ilahyany-mbp.local>
2024-12-02 17:29:59 +0200

    another try :)

--- MyString.java
+++ MyString.java
@@ -51,50 +51,87 @@ public class MyString {
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
 
-        // initiazling an array big enough to contain the indexes of all appearances of string 1 first letter in string 2.
         if (str2.length() == 0) {
-            return (str1.length() == str2.length() ? true : false);
-        }
-        char firstLetterOfStr2 = str2.charAt(0);
-        int[] indexArr = new int[countLetters(str1, firstLetterOfStr2)];
-
-        // checking if array length is 0, meaning no matches for first letter of str2 were found in str1 (countLetters returned 0)
-        if (indexArr.length == 0) {
-            return false;
-        }
-        
-        int IndexPointer = 0;
-        for (int w = 0; w < str1.length(); w++) {
-            // setting the array elements, one by one, to the indexes of the occurences of the first letter of str1 in str2.
-            if (str1.indexOf(firstLetterOfStr2, IndexPointer) != -1) { 
-                indexArr[w] = str1.indexOf(firstLetterOfStr2, IndexPointer);
-                // incrementing the pointer to the last index returned + 1, so in the next iteration we will skip it.
-                IndexPointer = str1.indexOf(firstLetterOfStr2, IndexPointer) + 1;
-            }
+            return true;
         }
 
-        boolean isContained = false;
-        int strLetterCount = 0;
+        int firstCharOfStr2 = str2.charAt(0);
+        int indexOfStr2InStr1 = str1.indexOf(firstCharOfStr2);
 
-        // each element in the array is an index of a potential match for str2, so we iterate over all elements and try to see if there's a match.
-        for (int i = 0; (i < indexArr.length); i++) {
-            strLetterCount = 0;
-            // initalizing a variable that will contain the current index in str1 that we are going to start from, just for the sake of readability.
-            int beginIndexOfStr1 = indexArr[i];
-   
-            // now we iterate over str2 and str1 and see if they match letter by letter.
-            // we also increment a letter counter that we will use later on to check if there was a full match.r
-            for (int s = 0; (s < str2.length()) && (s + beginIndexOfStr1 < str1.length()); s++) {
-                if (str1.charAt(beginIndexOfStr1 + s) == str2.charAt(s)) {
-                    strLetterCount++;
+        while (indexOfStr2InStr1 != -1) {
+            // checks if the substring starting at 'index' matches str2
+            boolean isMatch = true;
+
+            for (int i = 0; i < str2.length(); i++) {
+                if (indexOfStr2InStr1 + i >= str1.length() || str1.charAt(indexOfStr2InStr1 + i) != str2.charAt(i)) {
+                    isMatch = false;
+                    break;
                 }
             }
 
-            if (strLetterCount == str2.length()) {
-                isContained = true;
-                break;
+            // only if full match was found we will return true, otherwise we will continue checking the next appearance. 
+            if (isMatch) {
+                return true;
             }
+
+            indexOfStr2InStr1 = str1.indexOf(firstCharOfStr2, indexOfStr2InStr1 + 1);
         }
-        return isContained;
-    }   
+
+        // if no match was found, return false
+        return false;
+
+    }
 }
+
+
+
+
+
+
+// old version of contains function:
+
+// public static boolean contains(String str1, String str2) {
+
+//     // initiazling an array big enough to contain the indexes of all appearances of string 1 first letter in string 2.
+//     if (str2.length() == 0) {
+//         return true;
+//     }
+//     char firstLetterOfStr2 = str2.charAt(0);
+//     int[] indexArr = new int[countLetters(str1, firstLetterOfStr2)];
+
+//     // checking if array length is 0, meaning no matches for first letter of str2 were found in str1 (countLetters returned 0)
+//     if (indexArr.length == 0) {
+//         return false;
+//     }
+    
+//     int indexOfStr1Letter = str1.indexOf(firstLetterOfStr2);
+//     int arrIndex = 0; 
+//     while (indexOfStr1Letter != -1 && arrIndex < indexArr.length) {
+//         indexArr[arrIndex++] = indexOfStr1Letter;
+//         indexOfStr1Letter = str1.indexOf(firstLetterOfStr2, indexOfStr1Letter + 1);  
+//     }
+
+//     boolean isContained = false;
+//     int strLetterCount = 0;
+
+//     // each element in the array is an index of a potential match for str2, so we iterate over all elements and try to see if there's a match.
+//     for (int i = 0; (i < indexArr.length); i++) {
+//         strLetterCount = 0;
+//         // initalizing a variable that will contain the current index in str1 that we are going to start from, just for the sake of readability.
+//         int beginIndexOfStr1 = indexArr[i];
+
+//         // now we iterate over str2 and str1 and see if they match letter by letter.
+//         // we also increment a letter counter that we will use later on to check if there was a full match.r
+//         for (int s = 0; (s < str2.length()) && (s + beginIndexOfStr1 < str1.length()); s++) {
+//             if (str1.charAt(beginIndexOfStr1 + s) == str2.charAt(s)) {
+//                 strLetterCount++;
+//             }
+//         }
+
+//         if (strLetterCount == str2.length()) {
+//             isContained = true;
+//             break;
+//         }
+//     }
+//     return isContained;
+// }   
